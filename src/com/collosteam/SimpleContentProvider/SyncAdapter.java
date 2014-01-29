1060b21a64021c69572ff26b741fe4b19877a474
Andrew <collosteam@live.com>
2014-01-29 18:45:09 +0200

    Add Google Play Service Lib

--- src/com/collosteam/SimpleContentProvider/SyncAdapter.java
+++ src/com/collosteam/SimpleContentProvider/SyncAdapter.java
@@ -11,24 +11,18 @@ import android.util.Log;
 class SyncAdapter extends AbstractThreadedSyncAdapter {
     public static final String TAG = "SyncAdapter";
 
-
     public SyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
-
     }
-
 
     public SyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
         super(context, autoInitialize, allowParallelSyncs);
-
     }
-
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority,
                               ContentProviderClient provider, SyncResult syncResult) {
         Log.i(TAG, "Beginning network synchronization");
-
     }
 
 }
